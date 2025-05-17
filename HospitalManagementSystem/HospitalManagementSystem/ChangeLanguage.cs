e0f8941739bf962edca39e8679746db70aa1570e
seli-hom <efremselihom1@gmail.com>
2025-05-17 18:31:04 -0400

    added .bin and .resex to git ignore files and started class frob automatic i18n

---
+++ HospitalManagementSystem/HospitalManagementSystem/ChangeLanguage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Configuration;
+using System.Xml;
+namespace HospitalManagementSystem
+{
+    internal class ChangeLanguage
+    {
+        public void UpdateLanguage(string key, string value)
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+
+            foreach(XmlElement xmlElement in xmlDoc.DocumentElement)
+            {
+                if (xmlElement.Name.Equals("appSettings"))
+                {
+                    foreach(XmlNode xNode in xmlElement.ChildNodes)
+                    {
+                        if (xNode.Attributes[0].Value.Equals(key))
+                        {
+                            xNode.Attributes[1].Value = value;
+                        }
+                    }
+                }
+            }
+
+            ConfigurationManager.RefreshSection("appSettings");
+            
+            xmlDoc.Save(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+
+        }
+    }
+}
